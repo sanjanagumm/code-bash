70c7d4ee58d2db2f645ea7b3beafaf4b0b89bd4c
sanjanagumm <sanjana.gummuluru@gmail.com>
2024-10-19 23:14:36 +0530

    small change in latitude-longitude logic

--- analytics_dashboard/app.js
+++ analytics_dashboard/app.js
@@ -79,7 +79,10 @@ const generateCalls = async () => {
     const fixedLocations = [
         { latitude: 37.7749, longitude: -122.4194 },
         { latitude: 37.7750, longitude: -122.4195 },
-        { latitude: 37.7751, longitude: -122.4196 }
+        { latitude: 37.7751, longitude: -122.4194 },
+        { latitude: 37.7751, longitude: -122.4196 },
+        { latitude: 37.7750, longitude: -122.4194 },
+        { latitude: 37.7749, longitude: -122.4196 }
     ];
 
     const selectedLocation = fixedLocations[Math.floor(Math.random() * fixedLocations.length)];
